badf53ee061fff0aa6c33a28d03b5ce142d2f0a5
Peter <Peter@192.168.2.3>
2016-06-19 21:03:08 +0200

    Interface eruit gegooit en nu zou het moeten werken. Ik kijk er morgen weer naar.
    The Magier gaat nog wat leuks doen!

--- src/main/java/DAO/Hibernate/ArtikelDAOHibernate.java
+++ src/main/java/DAO/Hibernate/ArtikelDAOHibernate.java
@@ -6,68 +6,21 @@
 package DAO.Hibernate;
 
 import POJO.Artikel;
-import java.util.ArrayList;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import interfaceDAO.IArtikelDAO;
 
 /**
  *
  * @author Peter
  */
-public class ArtikelDAOHibernate implements IArtikelDAO {
-     private Session currentSession;
-    private Transaction currentTransaction;
-
-    public Session getCurrentSession() {
-        return currentSession;
-    }
-
-    public void setCurrentSession(Session currentSession) {
-        this.currentSession = currentSession;
-    }
-
-    public Transaction getCurrentTransaction() {
-        return currentTransaction;
-    }
-
-    public void setCurrentTransaction(Transaction currentTransaction) {
-        this.currentTransaction = currentTransaction;
-    }
-
-    public Session openCurrentSession(){
-        currentSession = getSessionFactory().openSession();
-        return currentSession;
-    }
+public class ArtikelDAOHibernate extends GenericDAO<Artikel, Integer> implements IArtikelDAO {
     
-    public Session openCurrentSessionWithTransaction(){
-        currentSession = getSessionFactory().openSession();
-        currentTransaction = currentSession.beginTransaction();
-        return currentSession;
-    }
-    
-    public void closeCurrentSession(){
-        currentSession.close();
-    }
-    
-    public void closeCurrentSessionWithTransaction(){
-        currentSession = getSessionFactory().openSession();
-        currentTransaction.commit();
-        currentSession.close();
-    }
-    
-    public SessionFactory getSessionFactory(){
-        StandardServiceRegistry ssrb = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        MetadataSources ms = new MetadataSources(ssrb);
-        ms.addAnnotatedClass(Artikel.class);
-        SessionFactory sf = ms.buildMetadata().buildSessionFactory();
-        return sf;
+    public ArtikelDAOHibernate() {
+        super(Artikel.class);
     }
 
+    /**
+     * Dit kan er uit
+     * 
     @Override
     public Artikel save(Artikel artikel) {
         getCurrentSession().save(artikel);
@@ -95,4 +48,5 @@ public class ArtikelDAOHibernate implements IArtikelDAO {
     public void update(Artikel artikel) {
        getCurrentSession().update(artikel);
     }
+    */
 }
